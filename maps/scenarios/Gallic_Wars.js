960e2fa7f4489e71d7bbecf5fa002cf090d28a6c
J.R.I.B.-Wein <Radagast@dragontale.de>
2014-08-11 02:00:36 +0200

    Shape the storyline. Add yes-no dialog, credit Sander.

--- maps/scenarios/Gallic_Wars.js
+++ maps/scenarios/Gallic_Wars.js
@@ -31,7 +31,15 @@ Trigger.prototype.storyline[INTRUDER_PLAYER] = {
 Trigger.prototype.messages = {}; // story telling
 Trigger.prototype.messages["start"] = function() 
 {
-	TriggerHelper.PushGUINotification([DEFENDER_PLAYER, INTRUDER_PLAYER], "54 B.C. All of Gaul has been subdued by Julius Caesar's Roman legionaires, known as 'The conquest of Gaul'.");
+	//TriggerHelper.PushGUINotification([DEFENDER_PLAYER, INTRUDER_PLAYER], "54 B.C. All of Gaul has been subdued by Julius Caesar's Roman legionaires, known as 'The conquest of Gaul'.");
+	var cmpGUIInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
+	cmpGUIInterface.PushNotification({
+		"players": [DEFENDER_PLAYER, INTRUDER_PLAYER], 
+		"message":
+			markForTranslation("54 B.C. All of Gaul has been subdued by Julius Caesar's Roman legionaires, known as 'The conquest of Gaul'."),
+		"translateMessage": true
+		
+	});
 }
 Trigger.prototype.messages["defend_village"] = function() 
 {
@@ -101,10 +109,51 @@ Trigger.prototype.enterConditions["subquest_rescue_le_druide"] = function()
 //======================================================================================
 Trigger.prototype.intro = function(data)
 {
-	warn('intro');
-	TriggerHelper.PushGUINotification([DEFENDER_PLAYER, INTRUDER_PLAYER],
-			"54 B.C. All of Gaul has been subdued by Julius Caesar's Roman legionaires, known as 'The conquest of Gaul'."
-	);
+	var cmpGUIInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
+	cmpGUIInterface.PushNotification({
+		"type": "dialog",
+		"players": [DEFENDER_PLAYER],
+		"dialogName": "yes-no",//Roman incursion!",
+		"data": {
+			"text": {
+				"caption": {
+					"message": markForTranslation("Are you up to the challenge to defend Gaul and you are older than 12?"),
+					"translateMessage": true,
+				},
+			},
+			"button1": {
+				"caption": {
+					"message": markForTranslation("I am."),
+					"translateMessage": true,
+				},
+				"tooltip": {
+					"message": markForTranslation("Accept the challenge to stop the Romans."),
+					"translateMessage": true,
+				},
+//	TODO Use OnPlayerCommand registered action!			"action": {
+//					"press": challengeAccepted()
+//				}
+				
+			},
+			"button2": {
+				"caption": {
+					"message": markForTranslation("Rather not"),
+					"translateMessage": true,
+				},
+				"tooltip": {
+					"message": markForTranslation("Decline the challenge and leave Gaul to the Romans."),
+					"translateMessage": true,
+				},
+//				"action": {
+//					"press": challengeDeclined()
+//				}
+				
+			},
+
+		},
+	});
+
+//	this.PushGUINotification([DEFENDER_PLAYER, INTRUDER_PLAYER],
 	cmpGUIInterface.PushNotification({
 		"players": [DEFENDER_PLAYER], 
 		"parameters": {"animalKind": "Boars"},
@@ -116,16 +165,105 @@ Trigger.prototype.intro = function(data)
 };
 
 
+////////////////////////////
+// TRIGGER/EVENT LISTENERS
+////////////////////////////
+Trigger.prototype.StructureBuiltAction = function(data)
+{
+	warn("The OnStructureBuilt event happened with the following data:");
+	warn(uneval(data));
+};
+
+Trigger.prototype.ConstructionStartedAction = function(data)
+{
+	warn("The OnConstructionStarted event happened with the following data:");
+	warn(uneval(data));
+};
+
+Trigger.prototype.TrainingFinishedAction = function(data)
+{
+	warn("The OnTrainingFinished event happened with the following data:");
+	warn(uneval(data));
+};
+
+Trigger.prototype.TrainingQueuedAction = function(data)
+{
+	warn("The OnTrainingQueued event happened with the following data:");
+	warn(uneval(data));
+};
+
+Trigger.prototype.ResearchFinishedAction = function(data)
+{
+	warn("The OnResearchFinished event happened with the following data:");
+	warn(uneval(data));
+};
+
+Trigger.prototype.ResearchQueuedAction = function(data)
+{
+	warn("The OnResearchQueued event happened with the following data:");
+	warn(uneval(data));
+};
+
+Trigger.prototype.OwnershipChangedAction = function(data)
+{
+	warn("The OnOwnershipChanged event happened with the following data:");
+	warn(uneval(data));
+};
+
+Trigger.prototype.PlayerCommandAction = function(data)
+{
+	warn("The OnPlayerCommand event happened with the following data:");
+	warn(uneval(data));
+	if (data.player == DEFENDER_PLAYER) 
+	{
+	}
+	if (data.cmd.type == 'dialog-answer')
+	{
+		if (data.cmd.answer == 'button2')
+		{
+			challengeDeclined();
+			this.DoAfterDelay(100, "victory", INTRUDER_PLAYER); // DEFENDER_PLAYER lost.
+		}
+		else 
+		{
+			challengeAccepted();
+			this.DoAfterDelay(100, "victory", DEFENDER_PLAYER); // TODO remove once further storyline is ready.
+		}
+
+	}
+};
+
+Trigger.prototype.RangeAction = function(data)
+{
+	// TODO Use ratio of own to enemy units as criterium.
+	if (data.currentCollection.length < this.TRESHOLD_ENEMY_COUNT_NEARBY_TO_HAVE_SUCCESSFULLY_DEFENDED)
+		this.leaveConditions["defend_village"] = true;
+	
+};
 
 Trigger.prototype.TreasureCollected = function(data)
 {
 };
 
+Trigger.prototype.IntervalAction = function(data)
+{
+	warn("The OnInterval event happened with the following data:");
+	warn(uneval(data));
+	this.numberOfTimerTrigger++;
+	if (this.numberOfTimerTrigger >= this.maxNumberOfTimerTrigger)
+		this.DisableTrigger("OnInterval", "IntervalAction");
+
+}
+
+
+///////////////////////////
+// CUSTOM TRIGGER ACTIONS (non-event bound)
+/////////////////////////// 
 Trigger.prototype.BattleMessage = function()
 {
 	var cmpGUIInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
 	cmpGUIInterface.PushNotification({
-		"players": [1,2], 
+		"players": [DEFENDER_PLAYER, INTRUDER_PLAYER], 
 		"message": markForTranslation("Defeat your enemy to win!"),
 		"translateMessage": true
 	});
@@ -135,10 +273,6 @@ Trigger.prototype.victory = function(playerID)
 {
 	TriggerHelper.SetPlayerWon(playerID);
 }
-
-
-
-
 
 
 
@@ -155,9 +289,35 @@ Trigger.prototype.UNIT_COUNT_REQUIRED_FOR_COUNTER_ATTACK = 100;
 cmpTrigger.state = "init";
 cmpTrigger.isAlreadyAchieved = {};
 cmpTrigger.activeEnemyAttacks = 0;
+cmpTrigger.numberOfTimerTrigger = 0;
+cmpTrigger.maxNumberOfTimerTrigger = 3; // on interval execution limit. If  the action is triggered more often then the trigger is being deactivated.
 
-// STORY EVENTS / TRIGGERS
-cmpTrigger.RegisterTrigger("OnTreasureCollected", "TreasureCollected", { "enabled": true });
+// SET UP STORY EVENTS / TRIGGERS
+var data = {"enabled": true};
+cmpTrigger.RegisterTrigger("OnConstructionStarted", "ConstructionStartedAction", data);
+cmpTrigger.RegisterTrigger("OnOwnershipChanged", "OwnershipChangedAction", data);
+cmpTrigger.RegisterTrigger("OnPlayerCommand", "PlayerCommandAction", data);
+cmpTrigger.RegisterTrigger("OnResearchFinished", "ResearchFinishedAction", data);
+cmpTrigger.RegisterTrigger("OnResearchQueued", "ResearchQueuedAction", data);
+cmpTrigger.RegisterTrigger("OnStructureBuilt", "StructureBuiltAction", data);
+cmpTrigger.RegisterTrigger("OnTrainingFinished", "TrainingFinishedAction", data);
+cmpTrigger.RegisterTrigger("OnTrainingQueued", "TrainingQueuedAction", data);
+cmpTrigger.RegisterTrigger("OnTreasureCollected", "TreasureCollected", data);
+
+data.delay = 5000; // after 5 seconds
+data.interval = 10000; // every 10 seconds
+cmpTrigger.numberOfTimerTrigger = 0;
+cmpTrigger.maxNumberOfTimerTrigger = 3; // execute it 3 times maximum
+cmpTrigger.RegisterTrigger("OnInterval", "IntervalAction", data);
+var entities = cmpTrigger.GetTriggerPoints("A");
+data = {
+	"entities": entities, // central points to calculate the range circles
+	"players": [1], // only count entities of player 1
+	"maxRange": 40,
+	"requiredComponent": IID_UnitAI, // only count units in range
+	"enabled": true,
+};
+cmpTrigger.RegisterTrigger("OnRange", "RangeAction", data);
 
 
 // STORY START
@@ -192,10 +352,10 @@ Trigger.prototype.storylineMachine = function(state_options)
 			var leaveCondition = this.leaveConditions[this.state]; // of the current state.
 			// Termination condition:
 			if (leaveCondition != undefined 
-					&& (leaveCondition == false  || typeof leaveCondition == 'object' && leaveCondition() == false))
+					&& (leaveCondition == false  || typeof leaveCondition == 'function' && leaveCondition() == false))
 			{
 				warn(this.state + " can't be left at this point, because you can't jump in the storyline. First solve your current task. TODO subquests being the exception. Subquests should work fully trigger based, i.e. there should not be a state for it. Actions/functions bound to the subquests should be marked as achieved when the final trigger action fires (i.e. the solving of the subquest).");
-				this.DoAfterDelay(2000, "storylineMachine", state_options); // check back in 1 second.
+				this.DoAfterDelay(4000, "storylineMachine", state_options); // check back in 1 second.
 				return ;
 			}
 			// Common enter condition: Never enter if a state/quest is already achieved/solved: (Trigger set this as achieved.)
@@ -206,7 +366,7 @@ Trigger.prototype.storylineMachine = function(state_options)
 			}
 				
 			var enterCondition = this.enterConditions[state_or_action]; 
-			if (!enterCondition || typeof enterCondition != 'object' || enterCondition())
+			if (!enterCondition || typeof enterCondition != 'function' || enterCondition())
 			{
 				// enter the state:
 				this.state = state_or_action;
@@ -214,17 +374,17 @@ Trigger.prototype.storylineMachine = function(state_options)
 				if (message)
 					if (typeof message == 'string') 
 						TriggerHelper.PushGUINotification(DEFENDER_PLAYER, message);
-					else if (typeof message == 'object')
+					else if (typeof message == 'function')
 						message();
 				warn('Entering state: ' + state_or_action);
 			   	this.storylineMachine(this.storyline[DEFENDER_PLAYER][state_or_action]);
 			}
 		}
 		// Is this a function (in this case more specific: a trigger action)?
-		else if (this[state_or_action])// && typeof this[state_or_action] == 'object')
+		else if (this[state_or_action] && typeof this[state_or_action] == 'function')
 		{
-			warn('Action: ' + state_or_action);
-			state_or_action();
+			warn('Action: ' + state_or_action + ' Typeof: ' + (typeof this[state_or_action]));
+			this[state_or_action]();
 			//this.DoAfterDelay(1000, state_or_action, {});
 		}
 		else
@@ -235,4 +395,12 @@ Trigger.prototype.storylineMachine = function(state_options)
 	
 }
 
+function challengeAccepted()
+{
+	warn('There is no time to loose. The Gaul we know is endangered.');
+}
+function challengeDeclined()
+{
+	warn('Caesar will be happy to hear, he has a new ally. Hopefully he does not have special plans with Gallic allies now that Gaul is lost.');
+}
 
